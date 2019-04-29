6a43c193deb38bf7470bfa1b175fe13078e1e022
Ryan Anjasmara <33214892+Hulahula6661@users.noreply.github.com>
2019-04-30 05:00:15 +0700

    Update The View

--- application/views/barcode_form.php
+++ application/views/barcode_form.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Barcode Generator Zend Library</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
+    <style type="text/css">
+    .vertical-center {
+        min-height: 100%;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+    </style>
 </head>
 <body>
-	<h1>Barcode Generator Using Zend Library</h1>
-	<form action="<?php echo site_url('Zend_Barcode_Example/set_barcode') ?>" method="POST">
-		<input type="text" name="code">
-		<button type="submit">Generate</button>
-	</form>
-
+    <center>
+        <div class="col-md-5 vertical-center">
+            <div class="container">
+                <!-- content  -->
+                    <h3>Barcode Generator Using Zend Library</h3>
+                    <p align="justify">Zend\Barcode provides a generic way to generate barcodes. The Zend\Barcode component is divided into two subcomponents: barcode objects and renderers. Objects allow you to create barcodes independently of the renderer. Renderer allow you to draw barcodes based on the support required. <a href="https://github.com/zendframework/zend-barcode">Source</a></p>
+                    <form action="<?php echo site_url('Main/generate') ?>" method="POST" autocomplete="off">
+                        <div class="form-group">
+                            <label for="host">Enter Code To Be Generated</label>
+                            <input class="form-control" type="text" name="code" placeholder="">
+                        </div>
+                        <button class="btn btn-primary">Generate</button>
+                    </form>
+            </div>
+        </div>
+    </center>
 </body>
 </html>
